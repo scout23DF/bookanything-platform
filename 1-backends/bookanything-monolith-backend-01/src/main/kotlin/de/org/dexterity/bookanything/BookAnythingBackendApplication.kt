b3386d93c3b9e9159c3b427120f8a6bb44d2b780
andre.nascimento <scout23df@gmail.com>
2026-09-13 22:58:05 +0200

    fix(health): disable and exclude elasticsearch health indicators for opensearch compatibility

--- 1-backends/bookanything-monolith-backend-01/src/main/kotlin/de/org/dexterity/bookanything/BookAnythingBackendApplication.kt
+++ 1-backends/bookanything-monolith-backend-01/src/main/kotlin/de/org/dexterity/bookanything/BookAnythingBackendApplication.kt
@@ -1,12 +1,20 @@
 package de.org.dexterity.bookanything
 
 import org.springframework.ai.autoconfigure.vertexai.gemini.VertexAiGeminiAutoConfiguration
+import org.springframework.boot.actuate.autoconfigure.data.elasticsearch.ElasticsearchReactiveHealthContributorAutoConfiguration
+import org.springframework.boot.actuate.autoconfigure.elasticsearch.ElasticsearchRestHealthContributorAutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cache.annotation.EnableCaching
 import org.springframework.data.web.config.EnableSpringDataWebSupport
 
-@SpringBootApplication(exclude = [VertexAiGeminiAutoConfiguration::class])
+@SpringBootApplication(
+	exclude = [
+		VertexAiGeminiAutoConfiguration::class,
+		ElasticsearchRestHealthContributorAutoConfiguration::class,
+		ElasticsearchReactiveHealthContributorAutoConfiguration::class
+	]
+)
 @EnableCaching
 @EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 class BookAnythingBackendApplication
